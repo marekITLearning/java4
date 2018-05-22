a87cf8fbc46a23367aa843236f6e4742c0a5cedc
Michal Hampel <miso.hampel@gmail.com>
2018-05-22 21:08:04 +0200

    update

---
+++ controller/src/main/java/sk/itlearning/java4/f/javafx/MainController.java
@@ -0,0 +1,41 @@
+package sk.itlearning.java4.f.javafx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainController {
+
+    @FXML
+    private Button btnLoad;
+
+    @FXML
+    private Button btnExport;
+
+    @FXML
+    private Button btnAdd;
+
+    @FXML
+    private Button btnEdit;
+
+    @FXML
+    void onBtnAdd(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onBtnEdit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onBtnExport(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onBtnLoad(ActionEvent event) {
+
+    }
+
+}
