38f9e16938443ed0b9f828b0ec5c4a7b37f4d377
Marek Ladicky <marek.ladicky@gmail.com>
2018-05-23 07:06:59 +0200

    update

--- controller/src/main/java/sk/itlearning/java4/a/xml/CatalogLoader.java
+++ controller/src/main/java/sk/itlearning/java4/a/xml/CatalogLoader.java
@@ -12,35 +12,5 @@ import sk.itlearning.java4.book.Catalog;
 
 public class CatalogLoader {
 
-	public static Catalog loadFullCatalog() {
-
-		// JAXB API for loading full xml content
-		try {
-			JAXBContext jaxbContext = JAXBContext.newInstance(Catalog.class);
-			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-			TimeUnit.SECONDS.sleep(5);
-			return (Catalog) jaxbUnmarshaller.unmarshal(Catalog.class.getResourceAsStream("book.xml"));
-		} catch (JAXBException | InterruptedException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-
-	public static boolean saveCatalog(Catalog catalog) {
-
-		// JAXB API for loading full xml content
-		if (catalog != null && !catalog.getBook().isEmpty()) {
-			try {
-				JAXBContext jaxbContext = JAXBContext.newInstance(Catalog.class);
-				Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-				jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-				jaxbMarshaller.marshal(catalog, new File("book.xml"));
-				return true;
-			} catch (JAXBException e) {
-				e.printStackTrace();
-			}
-		}
-		return false;
-	}
 
 }
