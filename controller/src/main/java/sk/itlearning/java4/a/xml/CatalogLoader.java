1ade5afa61bf364e90d0dd65566747b999ab1252
ladicky <ladicky@U523PL.itl.lan>
2018-05-23 10:01:50 +0200

    import cleanup and pom cleanup

--- controller/src/main/java/sk/itlearning/java4/a/xml/CatalogLoader.java
+++ controller/src/main/java/sk/itlearning/java4/a/xml/CatalogLoader.java
@@ -1,15 +1,5 @@
 package sk.itlearning.java4.a.xml;
 
-import java.io.File;
-import java.util.concurrent.TimeUnit;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
-
-import sk.itlearning.java4.book.Catalog;
-
 public class CatalogLoader {
 
 
