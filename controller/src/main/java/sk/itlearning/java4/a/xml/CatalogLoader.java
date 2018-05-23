10691c0e7960ba318aac999159c7fd7721eb0971
student <student@U523P02.itl.lan>
2018-05-23 11:35:39 +0200

    catalog xml reader

--- controller/src/main/java/sk/itlearning/java4/a/xml/CatalogLoader.java
+++ controller/src/main/java/sk/itlearning/java4/a/xml/CatalogLoader.java
@@ -1,6 +1,34 @@
 package sk.itlearning.java4.a.xml;
 
-public class CatalogLoader {
+import java.util.List;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import sk.itlearning.java4.book.Book;
+import sk.itlearning.java4.book.Catalog;
+
+public class CatalogLoader {
+	
+	public static void main(String[] args) {
+		Catalog c = getFullCatagalog();
+		
+		List<Book> catalogBooks = c.getBook();
+		
+		System.out.println(catalogBooks.size());
+	}
+
+	public static Catalog getFullCatagalog() {
+		try {
+			JAXBContext jaxbContext = JAXBContext.newInstance(Catalog.class);
+			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+			Catalog catalog = (Catalog) jaxbUnmarshaller.unmarshal(Catalog.class.getResourceAsStream("book.xml"));
+			return catalog;
+		} catch (JAXBException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
 }
