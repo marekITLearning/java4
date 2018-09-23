e42beb7f2752d4b76692fa90a905625096241c69
ladicky <ladicky@U408PL.itl.lan>
2018-09-23 14:25:01 +0200

    restwebapp

---
+++ restwebapp/src/main/java/sk/itlearning/java4/rest/page/Page.java
@@ -0,0 +1,27 @@
+package sk.itlearning.java4.rest.page;
+
+
+import java.time.LocalDateTime;
+
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("test")
+public class Page {
+
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	public Response doTest() {
+		JsonObjectBuilder jb = Json.createObjectBuilder();
+		
+		jb.add("Aktualny cas", LocalDateTime.now().toString());
+		
+		return Response.ok(jb.build()).build();
+	}
+	
+}
