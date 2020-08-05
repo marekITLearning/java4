35c3d86780c9f0ed2bc05f26ff17b76d036cb198
java-semiconductor <java.semiconductor@gmail.com>
2020-08-05 23:32:32 +0200

    update

--- app_jee_restapi/src/main/java/sk/itlearning/java4/rest/endpoints/ImdbData.java
+++ app_jee_restapi/src/main/java/sk/itlearning/java4/rest/endpoints/ImdbData.java
@@ -1,13 +1,15 @@
-package sk.itlearning.java4.rest.page;
+package sk.itlearning.java4.rest.endpoints;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
 @Path("imdb")
 public class ImdbData {
 
+	@GET
 	public Response getTopRated() {
-		return Response.ok().build();
+		return Response.ok("Hello World").build();
 	}
 
 }
