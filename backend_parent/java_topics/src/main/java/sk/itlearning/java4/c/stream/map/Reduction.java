ea46f5306cf5ec196d2f645a1c8e01fba610c221
student <student@U408PL.itl.lan>
2022-04-10 11:10:13 +0200

    update

--- backend_parent/java_topics/src/main/java/sk/itlearning/java4/c/stream/map/Reduction.java
+++ backend_parent/java_topics/src/main/java/sk/itlearning/java4/c/stream/map/Reduction.java
@@ -1,26 +1,43 @@
 package sk.itlearning.java4.c.stream.map;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import sk.itlearning.java4.a.xml.CatalogLoader;
+import sk.itlearning.java4.book.Book;
+
 public class Reduction {
 
-//	public static void main(String[] args) {
-//		
-//		List<Book> list = CatalogLoader.getFullCatagalog(CatalogLoader.class.getResourceAsStream("book.xml")).getBook();
-//		
-//		Optional<Book> ops = list.stream()
-//				.filter(e -> e.getPrice() < 20f)
-//				.max( (a, b) -> Float.valueOf(a.getPrice()).compareTo(Float.valueOf(b.getPrice())));
-//		
-//		ops.ifPresent(v -> v.setPrice(100));
-//		
-//		ops.ifPresent(v -> System.out.println(v.getId()));
-//		ops.ifPresent(v -> System.out.println(v.getPrice()));
-//		
-//		Book b = ops.orElse(new Book());
-//		
-//		ops.ifPresent(v -> System.out.println(v.getPrice()));
-//		
-//		Book b2 = ops.get();
-//		
-//	}
-	
+	public static void main(String[] args) {
+
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream(new File("C:\\Users\\student\\Desktop\\book.xml"));
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		}
+
+		List<Book> list = CatalogLoader.getFullCatagalog(fis).getBook();
+
+		Optional<Book> ops = list.stream().filter(e -> e.getPrice() < 20f)
+				.max((a, b) -> a.getPublishDate().compare(b.getPublishDate()));
+
+		try {
+			System.out.println(ops.get().getTitle());
+		} catch (NoSuchElementException e) {
+			System.out.println("Kniha do 2 dolarov nie je v zozname");
+		}
+
+		ops.ifPresent(v -> v.setPrice(100));
+		ops.ifPresent(v -> System.out.println(v.getPrice()));
+		
+		Book b = ops.orElse(new Book());
+		
+
+	}
+
 }
